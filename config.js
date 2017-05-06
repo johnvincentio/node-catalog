fa606a7041227bf7acbfb0b948edc5102c49d210
johnvincentio <john@johnvincent.io>
2017-05-06 15:44:00 -0400

    config

--- config.js
+++ config.js
@@ -5,5 +5,7 @@
 exports.DATABASE_URL = process.env.DATABASE_URL ||
                        global.DATABASE_URL ||
                       'mongodb://localhost/node-catalog';
-exports.TEST_DATABASE_URL = (process.env.TEST_DATABASE_URL || 'mongodb://localhost/test-node-catalog');
+exports.TEST_DATABASE_URL = (process.env.TEST_DATABASE_URL ||
+                             global.TEST_DATABASE_URL ||
+                             'mongodb://localhost/test-node-catalog');
 exports.PORT = process.env.PORT || 8080;
