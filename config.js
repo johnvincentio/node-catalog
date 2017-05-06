66ed4b30c3e1396e632ba3e9908e03de7e7a4f0c
johnvincentio <john@johnvincent.io>
2017-05-06 15:51:54 -0400

    test

--- config.js
+++ config.js
@@ -5,7 +5,11 @@
 exports.DATABASE_URL = process.env.DATABASE_URL ||
                        global.DATABASE_URL ||
                       'mongodb://localhost/node-catalog';
-exports.TEST_DATABASE_URL = (process.env.TEST_DATABASE_URL ||
+exports.JV_TEST_DATABASE_URL = (process.env.TEST_DATABASE_URL ||
                              global.TEST_DATABASE_URL ||
                              'mongodb://localhost/test-node-catalog');
+
+exports.TEST_DATABASE_URL =
+'mongodb://jvuser:leaf685@ds133331.mlab.com:33331/test-node-catalog';
+
 exports.PORT = process.env.PORT || 8080;
