87f1a554e0039d4bc5fc5cdae1d9cfe9b6e1e27f
johnvincentio <john@johnvincent.io>
2017-05-07 13:05:13 -0400

    test remote db

--- config.js
+++ config.js
@@ -5,7 +5,9 @@
 exports.DATABASE_URL = process.env.DATABASE_URL ||
                        global.DATABASE_URL ||
                       'mongodb://localhost/node-catalog';
-exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL ||
-                        'mongodb://localhost/test-node-catalog';
+//exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL ||
+//                        'mongodb://localhost/test-node-catalog';
+
+exports.TEST_DATABASE_URL = 'mongodb://localhost/test-node-catalog';
 
 exports.PORT = process.env.PORT || 8080;
